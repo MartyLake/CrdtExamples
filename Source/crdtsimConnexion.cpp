1e701970107802d2b3caae3ec16a82ac3af4da30
MartyLake <neogaldr@gmail.com>
2016-03-21 21:42:06 +0100

    Add Connexion constructor and getters.

--- Source/crdtsimConnexion.cpp
+++ Source/crdtsimConnexion.cpp
@@ -3,6 +3,10 @@
 
 namespace crdtsim
 {
+Connexion::Connexion (int sourceNodeId, int destinationId) : sourceNodeId (sourceNodeId), destinationId (destinationId) {}
+int Connexion::getSourceNodeIdentifier () const { return sourceNodeId; }
+int Connexion::getDestinationNodeIdentifier () const { return destinationId; }
+
 class TestConnexion : public juce::UnitTest
 {
 public:
@@ -10,7 +14,18 @@ public:
     void runTest ()
     {
         {
-            beginTest ("Normal use");
+            beginTest ("Create a connexion from node1 to node2");
+            auto random = juce::UnitTest::getRandom ();
+            auto nodeIdentifier1 = random.nextInt ();
+            auto nodeIdentifier2 = random.nextInt ();
+            while (nodeIdentifier1 == nodeIdentifier2)
+            {
+                nodeIdentifier2 = random.nextInt ();
+            }
+            expect (nodeIdentifier2 != nodeIdentifier1, "Bad initial conditions");
+            Connexion connexion{nodeIdentifier1, nodeIdentifier2};
+            expectEquals (connexion.getSourceNodeIdentifier (), nodeIdentifier1);
+            expectEquals (connexion.getDestinationNodeIdentifier (), nodeIdentifier2);
         }
     }
 } testTestConnexion;
--- Source/crdtsimConnexion.h
+++ Source/crdtsimConnexion.h
@@ -2,6 +2,15 @@
 #define CRDTSIMCONNEXION_H_INCLUDED
 namespace crdtsim
 {
+class Connexion
+{
+public:
+    Connexion (int sourceNodeId, int destinationId);
+    int getSourceNodeIdentifier () const;
+    int getDestinationNodeIdentifier () const;
 
+private:
+    int sourceNodeId, destinationId;
+};
 } //crdtsim
 #endif // CRDTSIMCONNEXION_H_INCLUDED
